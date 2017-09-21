93ef0a973025814837547d35f652bf671eabf306
talmeym <mtalmey@gmail.com>
2017-09-21 11:12:33 +0100

    renamed FileResponse CreateFileResponse

---
+++ src/test/java/com/emarte/regurgitator/test/CreateFileResponseJsonLoaderTest.java
@@ -0,0 +1,31 @@
+package com.emarte.regurgitator.test;
+
+import com.emarte.regurgitator.core.RegurgitatorException;
+import com.emarte.regurgitator.extensions.web.CreateFileResponseJsonLoader;
+import org.junit.Test;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+
+import static com.emarte.regurgitator.core.ConfigurationFile.loadFile;
+
+public class CreateFileResponseJsonLoaderTest extends JsonLoaderTest {
+	public CreateFileResponseJsonLoaderTest() {
+		super(new CreateFileResponseJsonLoader());
+	}
+
+	@Test
+	public void testThis() throws IOException, RegurgitatorException {
+		assertExpectation("classpath:/CreateFileResponse.json", "com.emarte.regurgitator.extensions.web.CreateFileResponse:['create-file-response-1',com.emarte.regurgitator.core.ValueSource:[com.emarte.regurgitator.core.ContextLocation:['request-metadata:path-info'],null],null]");
+	}
+
+	@Test
+	public void testPrefixed() throws SAXException, IOException, RegurgitatorException {
+		assertExpectation("classpath:/CreateFileResponse_prefixed.json", "com.emarte.regurgitator.extensions.web.CreateFileResponse:['create-file-response-1',com.emarte.regurgitator.core.ValueSource:[com.emarte.regurgitator.core.ContextLocation:['request-metadata:path-info'],null],'/assets']");
+	}
+
+	@Test
+	public void testFullLoadJson() throws RegurgitatorException {
+		loadFile("classpath:/CreateFileResponse_fullLoad.json");
+	}
+}
